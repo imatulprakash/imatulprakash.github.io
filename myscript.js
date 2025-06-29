1ef31ec630179286ef1c9ad8b4b93994d171d77d
Atul Prakash <pra.atul2@gmail.com>
2025-06-29 18:43:14 +0530

    focus on nav

---
+++ myscript.js
@@ -0,0 +1,13 @@
+function calculateAge() {
+    const [day, month, year] = "23/02/1989".split('/').map(Number);
+    const birthDate = new Date(year, month - 1, day);
+    const today = new Date();
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
